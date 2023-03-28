a846c3ec8aef9bf461c7de96289630fc0a02bd6c
fieryfalcon <rsudarsan369@gmail.com>
2023-03-28 18:31:31 +0530

    Login backend implemented

---
+++ streak/src/Components/Login.jsx
@@ -0,0 +1,76 @@
+import { initializeApp } from "firebase/app";
+import { React, useState, useEffect } from 'react';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "../Config/config.js";
+import { useSelector } from 'react-redux';
+import Cookies from 'universal-cookie';
+import { setUser } from '../redux/action';
+import { useDispatch } from 'react-redux';
+
+import { GoogleAuthProvider, signInWithPopup, GithubAuthProvider, signInWithRedirect } from "firebase/auth";
+import { collection, doc, setDoc, getDoc } from "firebase/firestore";
+
+export default function LoginPage() {
+    const dispatch = useDispatch();
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const firestore = getFirestore(app);
+    const user = useSelector((state) => state.user.user);
+    console.log(user);
+
+
+    const signInWithGoogle = async () => {
+        const provider = new GoogleAuthProvider();
+        provider.addScope("email");
+        const { user } = await signInWithPopup(auth, provider);
+        const uid = user.uid;
+
+        const userData = {
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            uid: user.uid,
+            time: "0:0",
+            email: user.providerData[0].email,
+            dos: [],
+            donts: [],
+            streakdata: [],
+        };
+
+
+
+
+
+        async function getUserData(uid) {
+            const docRef = doc(firestore, "users", uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+
+                const cookie = new Cookies();
+                cookie.set("user", docSnap.data());
+                console.log(cookie.get("user"));
+                dispatch(setUser(docSnap.data()));
+            } else {
+                console.log("No such document!");
+                return null;
+            }
+        }
+        const userDocRef = doc(collection(firestore, "users"), user.uid);
+        await setDoc(userDocRef, userData); // wait for the data to be written to Firestore
+        getUserData(uid);
+
+
+    };
+
+
+
+    return (
+        <div>
+            <button onClick={signInWithGoogle}>Login with Google</button>
+
+
+        </div>
+    );
+
+}
+
