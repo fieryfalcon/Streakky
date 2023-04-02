0d03d87e0ca90718c1af86065b75686300b8b2a7
fieryfalcon <rsudarsan369@gmail.com>
2023-04-03 04:09:57 +0530

    Typos fixed in about page

--- streak/src/Components/About.jsx
+++ streak/src/Components/About.jsx
@@ -5,10 +5,10 @@ const About = () => {
         <div id='about'>
             <h1 id='page2Heading'>About the App</h1>
             <div id='page2Content'>
-                "Welcome to my website! Do you struggle with maintaining habits? Our app can help
+                "Welcome to my website! Do you struggle with maintaining habits? This app can help
                 you build and maintain streaks of daily habits that will improve your overall well-being.
                 esearch shows that maintaining streaks can be highly effective in reinforcing positive
-                behaviors, building self-efficacy, and achieving challenging goals. By using our app to
+                behaviors, building self-efficacy, and achieving challenging goals. By using Streakky to
                 track and maintain your daily habits, you can experience the power of streaks and improve
                 your habits in a lasting way. Try it out today and see the benefits of maintaining streaks
                 for yourself!"
